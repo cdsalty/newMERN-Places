5ed9248b57cc32542d14a1a2f7104c84ea488d49
cdsalty <soltiscd@gmail.com>
2020-02-22 13:51:43 -0500

    added cards, avatars and styling logic

--- Places/react-places-frontend/src/user/components/UserItem.js
+++ Places/react-places-frontend/src/user/components/UserItem.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+import Avatar from '../../shared/components/UIElements/Avatar';
+import Card from '../../shared/components/UIElements/Card';
 import './UserItem.css';
 
 const UserItem = (props) => {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={props.image} alt={props.image} />
-        </div>
-        <div className="user-item__info">
-          <h2>{props.name}</h2>
-          <h3>
-            {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        {/* Link will reference a specific user's places*/}
+        <Link to={`/${props.id}/places`}>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+            </h3>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 };
@@ -25,4 +32,9 @@ export default UserItem;
 Note: how to display something is plural or not via a ternary expression
   - {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
 
-*/
+Link: has react-router-dom step in and block the link from actually rendering. It will go back and 
+render the router addresses specified. under the hood, it wraps an anchor tag but blocks navigation logic
+  - the link should reference the id of the user in order to load all the places of that user. it will dynamic.
+  - <Link to={`/${props.id}/places`}> when first used goes to localhost:3000/u1/places but doesn't reflect it in the url. 
+    - the actual url still shows localhost:3000
+  */
--- Places/react-places-frontend/src/user/components/UsersList.js
+++ Places/react-places-frontend/src/user/components/UsersList.js
@@ -1,31 +1,30 @@
 import React from 'react';
-import './UsersList.css';
+
 import UserItem from './UserItem';
+import './UsersList.css';
 
 const UsersList = (props) => {
   // if no users are in the system, display a title saying "No users found" else render a list of users and their details
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users were found</h2>
+        <h2>No users found.</h2>
       </div>
     );
   }
-  // if there are users in the system, then create an <ul> and inside, map over the 'items' and return details
+
   return (
-    <ul>
-      {props.items.map((user) => {
+    <ul className="users-list">
+      {props.items.map((user) => (
         // return a single user item for all users with their information details
-        return (
-          <UserItem
-            key={user.id}
-            id={user.id}
-            image={user.image}
-            name={user.name}
-            placeCount={user.places}
-          />
-        );
-      })}
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
     </ul>
   );
 };
