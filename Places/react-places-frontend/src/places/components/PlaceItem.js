c7156f7a60895fb7e3ff7c6ccab200efe6b5f22f
cdsalty <soltiscd@gmail.com>
2020-02-29 23:05:23 -0500

    working on rendering a list of each user's places, need routes

--- Places/react-places-frontend/src/places/components/PlaceItem.js
+++ Places/react-places-frontend/src/places/components/PlaceItem.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import Card from '../../shared/components/UIElements/Card';
 import './PlaceItem.css';
 
-const PlaceItem = (props) => {};
+// the layout, creating one layout to serve for each place;
+const PlaceItem = (props) => {
+  // don't forget to return
+  return (
+    <li className="place-item">
+      <Card className="place-item__content">
+        <div className="place-item__image">
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className="place-item__info">
+          <h2>{props.title}</h2>
+          <h3>{props.address}</h3>
+          <p>{props.description}</p>
+        </div>
+        <div className="place-item__actions">
+          <button>View In Map</button>
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      </Card>
+    </li>
+  );
+};
 
 export default PlaceItem;
--- Places/react-places-frontend/src/places/pages/UserPlaces.js
+++ Places/react-places-frontend/src/places/pages/UserPlaces.js
@@ -1,8 +1,46 @@
 // responsible for showing all the places of a user
 import React from 'react';
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_DATA_PLACES = [
+  {
+    id: 'p1',
+    title: 'San Francisco',
+    discription:
+      'The Golden Gate Bridge is a suspension bridge spanning the Golden Gate, the one-mile-wide strait connecting San Francisco Bay and the Pacific Ocean.',
+    imageUrl:
+      'https://cdn.britannica.com/s:800x450,c:crop/31/204431-138-B5008F47/time-lapse-video-San-Francisco.jpg',
+    address: 'Long Ave & Marine Dr, San Francisco, CA 94129',
+    location: {
+      lat: 37.8193198,
+      lng: -122.4720684
+    },
+    creator: 'u1' // later this will serve as the id of the user who created it
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    discription: 'One of the most famous sky scrappers in the world!',
+    imageUrl:
+      'https://s3.amazonaws.com/crowdriff-media/full/b2c44bc8bc8d231f2189ba380e64c5bfa1940a778bd9f5a4dfc617bfc04b79bd.jpg',
+    address: '20 W 34th Street, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u2' // later this will serve as the id of the user who created it
+  }
+];
 
 const UserPlaces = () => {
-  return;
+  return <PlaceList items={DUMMY_DATA_PLACES} />;
+  // this is why items is referred to as each component that is passed within/passed down;
 };
 
 export default UserPlaces;
+
+/*
+
+https://cdn.britannica.com/s:800x450,c:crop/31/204431-138-B5008F47/time-lapse-video-San-Francisco.jpg
+
+*/
